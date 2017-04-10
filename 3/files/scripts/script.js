f7b7d0e6d20e676caa5d23d16a3da66ecca6d458
Malyshko Pavel <malyshko.pavel@gmail.com>
2017-04-10 09:33:48 +0300

    nav complete

--- 3/files/scripts/script.js
+++ 3/files/scripts/script.js
@@ -2,22 +2,9 @@ $(document).ready(function(){
 
 	//Navigation
 
-	$("#nav-btn").click(function() {
-	  $("#nav-list").slideToggle(500);
-	  $("#nav-btn").toggleClass("nav-slide");
-  });
+	$("#mobile-btn").click(function() {
+	  $("#mobile-btn").toggleClass("mobile-btn-slide");
+	  $("#list").toggleClass("mobile-list-slide")
+  	});
 
-	$(window).resize(function() {
-		if ($(window).width() > "715") {
-			$("#nav-list").css("display", "block");
-	 	}else{
-	 		$("#nav-btn").removeClass("nav-slide");
-	 		$("#nav-list").css("display", "none");
-	 	}
-	});
-
-	//Slider
-
-	
-	
 });
